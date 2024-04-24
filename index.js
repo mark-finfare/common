42117b44091e068cf313d4cbe0df651a00892e71
Mark Ye <fengy@finfare.com>
2024-04-24 15:50:44 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,15 @@
+export const version = '0.0.0';
+
+export function sum(a, b) {
+  return a + b;
+}
+
+/**
+ * This is what this function does
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+export function typedSum(a, b) {
+  return a + b;
+}
---
+++ module.js
@@ -0,0 +1,4 @@
+export const fromModule = {
+  a: 1,
+  b: 2,
+};
